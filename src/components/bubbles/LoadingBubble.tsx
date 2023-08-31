da866ccdc46ba0d983c2190756f8b96c239b943f
Jedidiah Hurt <jedidiah@paloalto.com>
2023-08-30 19:55:09 -0700

    Customize theme

--- src/components/bubbles/LoadingBubble.tsx
+++ src/components/bubbles/LoadingBubble.tsx
@@ -1,14 +1,12 @@
-import { TypingBubble } from '@/components'
+import { TypingBubble } from "@/components";
 
 export const LoadingBubble = () => (
-  <div
-    class="flex justify-start mb-2 items-start animate-fade-in host-container"
-  >
+  <div class="flex justify-start mb-2 items-start animate-fade-in host-container">
     <span
-      class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble"
+      class="px-2 py-2 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble"
       data-testid="host-bubble"
     >
       <TypingBubble />
     </span>
   </div>
-)
+);
